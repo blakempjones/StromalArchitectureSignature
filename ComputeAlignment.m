7044bcb0a1aa3efaddeba6732399a494362102dc
Blake Jones <blakempjones@gmail.com>
2019-09-11 14:04:06 -0400

    Refactors ComputeAlignment and ComputeRSquared functions

--- ComputeAlignment.m
+++ ComputeAlignment.m
@@ -1,11 +1,32 @@
 function alignmentMatrix = ComputeAlignment(dirImage, nhoodSize)
-
-dirImage(isnan(dirImage)) = 0;
+%
+% Computes the maximum difference in the diretion of the optical axis for
+% the neighbourhood around each pixel. Used in the alignment score outlined
+% in: "Novel methodology to image stromal tissue and assess its 
+% morphological features with polarized light: towards a tumour 
+% microenvironment prognostic signature," Biomed. Opt. Express 10, 3963-3973 (2019)
+%
+% --- Input ---
+% dirImage: A 2D matrix where each pixel corresponds to the optical axis 
+% directional at that pixel.
+% nhoodSize: The number of pixels to use in the neighbourhood around a
+% pixel. For instance, a value of 2 would result in a 5x5 neighbourhood (2
+% + 2 + center Pixel = 5).
+% 
+% --- Output ---
+% alignmentMatrix: A 2D matrix of the same size as dirImage whose values
+% correspond to the maximum angular difference found in the neighbourhood
+% of each pixel.
+%
 
 imgSize = size(dirImage);
 
-changeMatrix = 10*ones(imgSize);
+dirImage(isnan(dirImage)) = 0;
 
+% Because of the periodic nature of the angle measurements (i.e. 90 = 0) we
+% need to use a circular number space when computing the maximum
+% difference. Otherwise the difference between 85 and 0 (5 degrees) will be
+% over-represented.
 adjustedImage = dirImage;
 adjustedImage(adjustedImage <= 9) = adjustedImage(adjustedImage <= 9) + 18;
 adjustedImage = adjustedImage - 9;
@@ -15,17 +36,23 @@ alignmentMatrix = zeros(imgSize);
 for i = 1:imgSize(1)
     
     for j = 1:imgSize(2)
-        
+        % Find the max difference in the neighbourhood with the original
+        % scaling (i.e. difference between 0 and 85 degrees will be 85).
         roi = dirImage(max(1, i - nhoodSize): min(imgSize(1), i + nhoodSize), max(1, j - nhoodSize): min(imgSize(2), j + nhoodSize));
+        % Find the max difference in the neighbourhood with the recentered
+        % scaling (difference betwen 0 and 85 is now 5 but the
+        % discontinuity still exists).
         roi2 = adjustedImage(max(1, i - nhoodSize): min(imgSize(1), i + nhoodSize), max(1, j - nhoodSize): min(imgSize(2), j + nhoodSize));
+        % Take the min of the 2 roi variables, correctly representing the
+        % differences.
         alignmentMatrix(i,j) = min(std(roi, 0, 'all'), min(std(roi2, 0, 'all')));
-        
     end
     
 end
 
+% Rescale the alignmentMatrix such that the higher the numerical value of
+% the alignment metric, the more aligned a region is.
 alignmentMatrix(alignmentMatrix == 0) = max(alignmentMatrix, [], 'all') + 1;
-
 alignmentMatrix = -1 * alignmentMatrix;
 
 end
--- ComputeRSquared.m
+++ ComputeRSquared.m
@@ -1,63 +1,76 @@
-function rSquared = ComputeRSquared(sample)
+function rSquared = ComputeRSquared(sample, startAngle, endAngle, stepSize)
+%
+% Computes the correlation with the sinusoidal pattern expected from
+% polarization theory for a stack of PLM images, each offset by a constant 
+% degree from their predecessor.
+%
+% --- Input ---
+% sample: a 3D matrix representation of an image stack where the third
+% dimension corresponds to respective images.
+% startAngle: Tau angle at which imaging began. For this code the choice of
+% angles can be arbitrary so 1 should typically be used.
+% endAngle: Tau angle at which imaging ended. 90 is the typical value.
+% stepSize: Incremental difference used in the calculation of the
+% theoretical curve.
+%
+% --- Output ---
+% rSquared: parametric image of R squared values.
+%
 
+imageSize = size(sample);
+
+% Type and nan cleaning
 normalized = single(sample);
-
 normalized(isnan(normalized)) = 0;
 
-imageSize = size(normalized);
-
+% Normalization of the sample to be between 1 and -1
 mins = min(normalized, [], 3);
-
 normalized = normalized - mins;
-
 maxs = max(normalized, [], 3);
-
 normalized = normalized ./ maxs;
-
 normalized = normalized - 0.5;
-
 normalized = normalized .* 2;
 
-param1 = 1;
-param2 = 4;
-x90 = 1:0.01:90;
-param3Expected = param1 * sind(param2 * x90);
+% Frequency parameter, known from theory.
+param2 = 2;
+
+% Values to compute expected sine curve
+x90 = startAngle : stepSize : endAngle;
+
+% Horizontal translation parameter of the expected curve
+param3Expected = sind(param2 * x90).^2;
 param3Expected = x90(find(param3Expected == max(param3Expected), 1));
 
-% xSample = (1:5:90)';
-xSample = linspace(1,90, imageSize(3));
+% Values at which the PLM was sampled (i.e. imaged)
+xSample = linspace(startAngle, endAngle, imageSize(3));
 
+% Holder for the horizontal translation parameter of the data
 param3 = zeros(imageSize(1), imageSize(2));
 
 for i = 1 : imageSize(1)
     
     for j = 1 : imageSize(2)
         
+        % Calculate the horizontal translation via comparison of where the
+        % max value occured
         maxValX = xSample(find(normalized(i,j,:) == 1, 1));
-        
         param3(i,j) = maxValX - param3Expected;
         
     end
     
 end
 
+% Create a matrix the same size as the input and store, along the 3rd
+% dimension the expected results (this time calculated using the correct
+% horizontal translation.
 xSampleReshaped = reshape(xSample, 1, 1, length(xSample));
 expectedResults = repmat(xSampleReshaped,imageSize(1), imageSize(2));
-% expectedResultsNoShift = expectedResults;
 expectedResults = expectedResults - param3;
+expectedResults = sind(param2 .* expectedResults).^2;
 
-% expectedResults = exp(-expectedResultsNoShift/50) .* sind(param2 .* expectedResults);
-expectedResults = sind(param2 .* expectedResults);
-
-% maxs = max(expectedResults, [], 3);
-% expectedResults = expectedResults ./ maxs;
-% expectedResults = (expectedResults - 0.5)*2;
-
-
+% Compute the residuals and final rSquared value.
 SStot = sum((expectedResults - mean(expectedResults, 3)).^2,3);
-
 SSres = sum((normalized - expectedResults).^2,3);
-
 rSquared = 1 - (SSres ./ SStot);
 
 end
